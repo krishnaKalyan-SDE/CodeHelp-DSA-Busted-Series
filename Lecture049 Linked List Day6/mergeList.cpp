8e351a330e71dd1b3b89428edc256d6e66db8183
Krishna_Kalyan <krishnakalyan833@gmail.com>
2025-01-19 16:49:24 +0530

    Update mergeList.cpp

--- Lecture049 Linked List Day6/mergeList.cpp
+++ Lecture049 Linked List Day6/mergeList.cpp
@@ -42,17 +42,26 @@ void solve(Node<int>* first, Node<int>* second) {
             curr2 = next2;
         }
         else {
-            
+            curr1=next1;
+            next1=next1->next;
+            if(next1==NULL){
+                curr1->next=curr2;
+                return first;   
+            }
         }
         
         
     }
     
-    
+    return first;
 }
 
 Node<int>* sortTwoLists(Node<int>* first, Node<int>* second)
 {
+    if(first->next==NULL){
+        first->next=second;
+        return first;
+    }
     if(first == NULL)
         return second;
     
